c76d1dd86c2f33860e15cda8b74ac6b2e05433b3
RustlingF <espin.7643@hotmail.com>
2016-09-02 14:17:52 -0700

    Finished my art

--- APLesson_01/ASCiiart.java
+++ APLesson_01/ASCiiart.java
@@ -2,11 +2,11 @@ public class ASCiiart
 {
 	public static void main(String[]args)
 	{
-		System.out.println(" (_)(_)");
-		System.out.println("  /     \\");
-		System.out.println(" /       |");
-		System.out.println(" (__ __)//");
-		System.out.println("");
+		System.out.println(" H     H  EEEEE L       L          OOO       W     W    OOO    RRRR   L        DDDD  !!");
+		System.out.println(" H     H  E         L       L         O     O      W W W   O    O   R     R   L        D       D !! ");
+		System.out.println("HHHHH EEEEE L       L         O     O      W W W   O    O   RRRR    L         D       D !!");
+		System.out.println("H      H  E         L       L         O     O  ,,    W W    O    O    R    R     L         D       D");
+		System.out.println("H     H   EEEEE LLLLL LLLLL   OOO  ,,     W W     OOO    R     R    LLLLL   DDDD  !!");
 		System.out.println("");
 		System.out.println("");
 	}
