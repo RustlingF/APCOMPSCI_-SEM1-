8d6a9703c67914cc01f50b19210b6584dc68af1e
RustlingF <espin.7643@hotmail.com>
2016-09-02 13:27:15 -0700

    Added files

---
+++ APLesson_01/ASCiiart.java
@@ -0,0 +1,13 @@
+public class ASCiiart
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" (_)(_)");
+		System.out.println("  /     \\");
+		System.out.println(" /       |");
+		System.out.println(" (__ __)//");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
