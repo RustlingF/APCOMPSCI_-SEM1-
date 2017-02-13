df24e229a5e73b759b9123ffb14ccf8d9fd16260
RustlingF <espin.7643@hotmail.com>
2017-02-13 14:14:26 -0800

    changes

--- APLesson_13/ToyStore.java
+++ APLesson_13/ToyStore.java
@@ -1,20 +1,20 @@
-
+import java.util.Scanner; 
 import java.util.ArrayList; 
 public class ToyStore 
 {
-	ArrayList<toy> toyList = new ArrayList<toy>();
+	private Arraylist<Toy> toyList;
 	
-	public Toy1s()
-	{
-		toyList.add(new Car(""));
-		toyList.add(new AFigure(""));
-	}
-	public Toy1s(String list)
-	{
-		loadToys(list)
-	}
+		public ToyStore()
+		{
+			ArrayList<Toy> toyList = new ArrayList <Toy>(); 
+		}
+		public ToyStore(String ts)
+		{
+			ArrayList<Toy> toyList = new ArrayList<Toy>();
+			loadToys(ts); 
+		}
 	
-	public loadToys(String ts)
+	public void loadToys(String ts)
 	{
 		String[] toys = ts.split(", ");
 		for(int i=0;i<toys.length;i++)
@@ -36,10 +36,71 @@ public class ToyStore
 		}
 	}
 	
-	public getThatToys(String nm)
+	public String getThatToys(String nm)
 	{
-		for(Toy:toyList)
+		for(Toy toy: toyList)
+		{
+			if(name == nm)
+			{
+				return Toys ; 
+			}
+			
+			else
+			{
+				return null ;
+			}
+		}
 	}
 	
+	public String getMostFrequentToy()
+	{
+		String name = "";
+		int max = Integer.MIN_VALUE;
+		
+		for(Toy toy:toyList)
+		{
+			if(max < count)
+				max = Toy.getCount;
+				name = Toy.getName;
+		}
+		return
+			name;
+	
+	
+	}
+	
+	public String getMostFrequentType()
+	{
+		int cars = 0 ;
+		int figures = 0 ;
+		
+		for(Toy toy:toyList)
+		{
+			if(Toy == Car)
+			{
+				cars+=1;
+			}
+			if(Toy == AFigure)
+			{
+				figures += 1 ;
+			}
+		}
+		if(cars > figures)
+		{
+			return cars;
+		}
+		if(figures > cars)
+		{	
+			return figures;
+		}
+		else
+		{
+			return "Equal amounts of action figures and cars!"; 
+		}
+	}
+	public String toString()
+	{
+		return toyList;
+	}
 	
 }
